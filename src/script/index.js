a39576f725812ea36d1715b20184f8c4bc66621d
anuragFE2026 <anurag.singh@joshtechnologygroup.com>
2026-07-04 20:05:24 +0530

    [AS_A1_02]: Completed navbar with responsiveness

--- src/script/index.js
+++ src/script/index.js
@@ -25,7 +25,7 @@ function handleActionBtns() {
     const actions = document.querySelector('.header__actions');
     const navMenu = document.querySelector('.header__nav');
     const container = document.querySelector('.header__container');
-    const mobile = window.matchMedia('(max-width: 828px)');
+    const mobile = window.matchMedia('(max-width: 760px)');
 
     /**
      * Move or restore the action buttons for the current screen size.
